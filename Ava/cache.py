cb748aba4ebb0ce5202ed5533ab4b0513cd2030b
Maxime Barbier <maxime.barbier1991@gmail.com>
2020-03-15 13:37:42 +0100

    add partial search

--- Ava/cache.py
+++ Ava/cache.py
@@ -1,6 +1,7 @@
 import logging
-from Ava.stem import steam_sentence
+from typing import List
 
+from Ava.stem import steam_sentence
 
 logger = logging.getLogger(__package__)
 
@@ -8,29 +9,28 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class _WithName:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
 
     def __str__(self):
         return self.name
 
 class Event(_WithName):
-    def trigger(self):
+    def trigger(self) -> None:
         print("Trigger event <%s>" % self.name)
 
 class EventList:
     def __init__(self):
         self._events = []
 
-    def add(self, event):
-        assert isinstance(event, Event)
+    def add(self, event: Event) -> None:
         self._events.append(event)
 
-    def trigger(self):
+    def trigger(self) -> None:
         for e in self._events:
             e.trigger()
 
-    def display(self, tab=0):
+    def display(self, tab: int=0) -> None:
         for e in self._events:
             print("%s[Event] <%s>" % ("\t" * tab, e))
 
@@ -43,13 +43,13 @@ class _CacheNode(_WithName):
         self._leaf = EventList()
         self._nodes = {} # str, _CacheNode
 
-    def display(self, tab=0):
+    def display(self, tab: int=0) -> None:
         print("%s [Node] <%s>" % ("\t" * tab, self.name))
         self._leaf.display(tab + 1)
         for k, v in self._nodes.items():
             v.display(tab + 1)
 
-    def add(self, tokens, event):
+    def add(self, tokens: List[int], event: Event) -> None:
         logger.debug("Add tokens %s", tokens)
         if not tokens:
             logger.debug("Associate event <%s>", event)
@@ -59,51 +59,57 @@ class _CacheNode(_WithName):
             self._nodes.setdefault(token, _CacheNode(token))
             self._nodes[token].add(tokens[1:], event)
 
-    def get(self, tokens):
+    def get(self, tokens: List[int], longest: bool=False) -> EventList:
         if not tokens:
             return self._leaf
+
         try:
             token = tokens[0]
-            return self._nodes[token].get(tokens[:1])
+            return self._nodes[token].get(tokens[1:])
         except KeyError:
             logger.debug("No key %s", token)
-            logger.debug(self._nodes.keys())
+            if longest:
+                return self._leaf
             return EventList()
 
 class Cache:
     def __init__(self):
         self._root = {}
 
-    def add(self, sentence, event):
-        assert sentence
+    def add(self, sentence: str, event: Event) -> None:
         tokens = list(steam_sentence(sentence))
         logger.debug("Add tokens: %s", tokens)
         token = tokens[0]
         self._root.setdefault(token, _CacheNode(token))
         self._root[token].add(tokens[1:], event)
 
-    def get(self, sentence):
-        assert sentence
+    def get(self, sentence: str, longest: bool=False) -> EventList:
         tokens = list(steam_sentence(sentence))
         token = tokens[0]
         try:
-            return self._root[token].get(tokens[1:])
+            return self._root[token].get(tokens[1:], longest=longest)
         except KeyError:
             logger.debug("No key %s", token)
             return EventList()
 
-    def display(self):
+    def display(self) -> None:
         for k, v in self._root.items():
             v.display()
 
 
 
+if __name__ == "__main__":
+    cache = Cache()
+    cache.add("la", Event("la"))
+    cache.add("la nuit", Event("la nuit"))
+    cache.add("la nuit tombe", Event("la nuit tombe"))
+    cache.add("la nuit se lève", Event("la nuit se lève"))
 
-cache = Cache()
-cache.add("la nuit tombe", Event("la nuit tombe"))
-cache.add("la nuit se lève", Event("la nuit se lève"))
-cache.display()
-print(cache.get("la nuit tombe"))
-print(cache.get("la nuit se lève"))
-print(cache.get("la journée est belle"))
-print(cache.get("le jour est beau"))
+    cache.display()
+
+    cache.get("la nuit tombe").display()
+    cache.get("la nuit se lève").display()
+    cache.get("la nuit").display()
+
+    cache.get("la journée est belle").display()
+    cache.get("le jour est beau").display()
