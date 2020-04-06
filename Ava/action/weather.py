a78e05944269215db8045a47428749373009b46f
maxime barbier <maxime.barbier@lamark.fr>
2020-04-06 10:42:03 +0200

    update info for meteo

--- Ava/action/weather.py
+++ Ava/action/weather.py
@@ -1,5 +1,5 @@
 import logging
-
+import re
 from meteofrance.client import meteofranceClient, meteofranceError
 
 from Ava.core import (
@@ -10,6 +10,27 @@ from Ava.core import (
 logger = logging.getLogger(__name__)
 
 
+def day_to_int(day, data):
+    if isinstance(day, int):
+        return day
+    res = 0
+    if day == "demain":
+        res = 1
+    elif day == "après demain":
+        res = 2
+    else:
+        m = re.match("dans (\d)+ jour[s]?", day)
+        if m:
+            res = int(m.group(1))
+        else:
+            d = day[:3]
+            for key, value in data.items():
+                if value["date"][:3] == d:
+                    res = key
+                    break
+    return res
+
+
 class WeatherAction(Action, TTSMixin):
     def __init__(self, ava, *args, city=None, day=0, **kwargs):
         Action.__init__(self, ava, *args, **kwargs)
@@ -18,19 +39,23 @@ class WeatherAction(Action, TTSMixin):
 
     def _do_trigger(self, *args, city=None, day=0, **kwargs) -> None:
         city = city or self._city
-        day = day or self._day
         try:
             client = meteofranceClient(city, True, include_today=True)
             data = client.get_data()
+            day = day_to_int(day or self._day, data["forecast"])
             logger.debug("Weather data for city=%s day=%s: %s", city, day, data)
-            day_str= "aujourd'hui" if day==0 else "dans %s jours" % day
-            msg = "La météo à %s pour %s. " % (data["name"], day_str)
+            day_str= "aujourd'hui" if day == 0 else "dans %s jours" % day
+
+            msg = "La météo à %s pour %s: " % (data["name"], day_str)
             weather = data["forecast"][day]
+
             if day == 0:
                 msg += "Le temps est %s et la température %s" % (
                     data["weather"],
                     data["temperature"]
                 )
+                if "rain_forecast_text" in data:
+                    msg += ". " + data["rain_forecast_text"]
             else:
                 msg += weather["weather"]
 
@@ -38,8 +63,8 @@ class WeatherAction(Action, TTSMixin):
                 weather["min_temp"],
                 weather["max_temp"]
             )
-        except meteofranceError:
-            logger.error("Impossible to get weather for city=%s, day=%s", city, day)
+        except Exception as e:
+            logger.exception("Impossible to get weather for city=%s, day=%s", city, day, exc_info=e)
             msg = "impossible to get weather informations"
 
         self.say(msg)
