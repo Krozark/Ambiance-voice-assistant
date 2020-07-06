e3d959a5af771955af8a6d69da2a51da254bf0ca
maxime barbier <maxime.barbier@lamark.fr>
2020-07-06 10:18:38 +0200

    add files

--- data/music/auto-table-top/audio-table-top.py
+++ data/music/auto-table-top/audio-table-top.py
@@ -6,9 +6,10 @@ import requests
 filename = sys.argv[1]
 data = json.load(open(filename, "r"))
 
+i = 0
 for x in data["log"]['entries']:
     for h in x["response"]["headers"]:
-        if h["value"] == "application/ogg":
+        if h["value"] in ("application/ogg", "audio/ogg"):
             url = x["request"]["url"]
             filename = url.rsplit("/", 1)[-1]
             print("Download %s" % url)
@@ -17,3 +18,5 @@ for x in data["log"]['entries']:
                 with open(filename, "wb") as f:
                     for chunk in r.iter_content(chunk_size=8192):
                         f.write(chunk)
+            i += 1
+print("Download %s files" % i)
