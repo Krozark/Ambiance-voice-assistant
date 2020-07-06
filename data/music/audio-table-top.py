22707b45d3d7b925b8e91ec4bf2a5033707c68b9
maxime barbier <maxime.barbier@lamark.fr>
2020-07-06 10:02:11 +0200

    add tabletop audio downloader

---
+++ data/music/audio-table-top.py
@@ -0,0 +1,19 @@
+#!/bin/env python
+import json
+import sys
+import requests
+
+filename = sys.argv[1]
+data = json.load(open(filename, "r"))
+
+for x in data["log"]['entries']:
+    for h in x["response"]["headers"]:
+        if h["value"] == "application/ogg":
+            url = x["request"]["url"]
+            filename = url.rsplit("/", 1)[-1]
+            print("Download %s" % url)
+            with requests.get(url, stream=True) as r:
+                r.raise_for_status()
+                with open(filename, "wb") as f:
+                    for chunk in r.iter_content(chunk_size=8192):
+                        f.write(chunk)
